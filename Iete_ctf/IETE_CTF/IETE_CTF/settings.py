2e6e0f3e379a96d8a929c3cef5b50a032e3c6eaf
Reyansh17 <gahlotreyansh17@gmail.com>
2024-04-16 21:46:41 +0530

    mcccccccccccc

--- Iete_ctf/IETE_CTF/IETE_CTF/settings.py
+++ Iete_ctf/IETE_CTF/IETE_CTF/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9p$hk*4y-arh32gfsnnhhx$wral@@*(vvzt%qaj5a%rr5ikc#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
