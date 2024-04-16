ef24d08f9ff11a303a75b5b9753366beb8e8e856
Reyansh17 <gahlotreyansh17@gmail.com>
2024-04-16 21:04:39 +0530

    kuch nahi

--- Iete_ctf/IETE_CTF/CTF/views.py
+++ Iete_ctf/IETE_CTF/CTF/views.py
@@ -24,137 +24,105 @@ def about(request):
     return render(request, 'ctf/about.html')
 
 @csrf_exempt
-def inst(request):
+def instructions(request):
     return render(request, 'ctf/instructions.html')
 
 @csrf_exempt
 def hint(request):
     if request.method == 'POST':
-        question = Questions.objects.get(Qid=request.POST.get('id'))
-        hint = question.Hint
-        questionPoints = question.points
-        user = User.objects.get(username=request.user.username)
-        userprofile = UserProfile.objects.get(user=user)
+        question_id = request.POST.get('id')
         try:
-            solved = Submission.objects.filter(question=question, user=userprofile)
+            question = Questions.objects.get(Qid=question_id)
+            hint = question.Hint
+            question_points = question.points
+            user = User.objects.get(username=request.user.username)
+            user_profile = UserProfile.objects.get(user=user)
+            solved = Submission.objects.filter(question=question, user=user_profile).exists()
+            if not solved:
+                user_profile.score -= question_points * 0.1
+                user_profile.save()
+                Submission.objects.create(question=question, user=user_profile, curr_score=user_profile.score)
             return HttpResponse(hint)
-        except Submission.DoesNotExist:
-            solved = Submission()
-            userprofile.score -= questionPoints * 0.1
-            solved.question = question
-            solved.user = userprofile
-            solved.curr_score = userprofile.score
-            solved.save()
-            userprofile.save()
-            return HttpResponse(hint)
-    return render(request, 'ctf/404.html')
+        except Questions.DoesNotExist:
+            return HttpResponse("Question not found")
+    return HttpResponse("Method not allowed")
 
 @csrf_exempt
 def check(request):
-    user = User.objects.get(username=request.user.username)
-    userprofile = UserProfile.objects.get(user=user)
-    questions = Questions.objects.all().order_by('Qid')
     if request.method == 'POST':
-        req = request.POST
-        Qid = req.get('Qid')
-        flag = req.get('flag')
-        level = req.get('customRadio')
-        quest = Questions.objects.get(Qid=int(Qid))
-        quest.Qid = Qid
-        if level == None:
-            return HttpResponse("-1")
-        else:
-            quest.level = level
+        user = User.objects.get(username=request.user.username)
+        user_profile = UserProfile.objects.get(user=user)
+        qid = request.POST.get('Qid')
+        flag = request.POST.get('flag')
+        level = request.POST.get('customRadio')
+
+        try:
+            question = Questions.objects.get(Qid=qid)
+            if level not in ['E', 'M', 'H']:
+                return HttpResponse("-1")
+            question.level = level
             if level == 'E':
-                quest.Easy += 1
+                question.Easy += 1
             elif level == 'M':
-                quest.Med += 1
+                question.Med += 1
             else:
-                quest.Hard += 1
-            print(quest.Easy, quest.Med, quest.Hard)
-            quest.save()
+                question.Hard += 1
+            question.save()
 
-            solved = Submission.objects.filter(question=quest, user=userprofile)
+            solved = Submission.objects.filter(question=question, user=user_profile).exists()
 
-            if flag == quest.flag:
+            if flag == question.flag:
                 if not solved:
-                    solved = Submission()
-                    userprofile.score += quest.points
-                    solved.question = quest
-                    solved.user = userprofile
-                    solved.curr_score = userprofile.score
-
-                    sec = calc()
-                    sec = duration - sec
-                    solved.sub_time = time.strftime("%H:%M:%S", time.gmtime(sec))
-                    userprofile.latest_sub_time = solved.sub_time
-                    # solved.sub_time = '{}:{}:{}'.format(hour, min, sec)
-                    print(solved.sub_time)
-                    quest.solved_by += 1
-                    solved.solved = 1
-                    userprofile.totlesub += 1
-                    userprofile.save()
-                    solved.save()
-                    quest.save()
-                    print(userprofile.score)
-                    print("FLAG IS CORRECT!")
+                    user_profile.score += question.points
+                    user_profile.save()
+                    Submission.objects.create(question=question, user=user_profile, curr_score=user_profile.score)
                     return HttpResponse('1')
-
                 else:
                     return HttpResponse('2')
             else:
-                print("INCORRECT")
                 return HttpResponse('0')
-    return HttpResponse("")
+        except Questions.DoesNotExist:
+            return HttpResponse("Question not found")
+    return HttpResponse("Method not allowed")
 
 @csrf_exempt
 def timer(request):
-    start = datetime.datetime.now()
-    starttime = start.hour * 60 * 60 + start.minute * 60 + start.second
-    global duration
+    start_time = datetime.datetime.now()
     global endtime
-    endtime = starttime + int(duration)
-    print(starttime)
-    return start
+    endtime = start_time + datetime.timedelta(seconds=duration)
+    return HttpResponse(start_time)
 
 @csrf_exempt
 def calc(request):
-    global endtime
     now = datetime.datetime.now()
-    nowsec = now.hour * 60 * 60 + now.minute * 60 + now.second
-    diff = endtime - nowsec
-    print(diff)
-    if nowsec <= endtime:
-        return diff
+    if now <= endtime:
+        diff = endtime - now
+        return HttpResponse(diff.total_seconds())
     else:
-        return 0
+        return HttpResponse("0")
 
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
-        recid = request.POST.get('reciept_id')
+        rec_id = request.POST.get('receipt_id')
         username = request.POST.get('username')
         password = request.POST.get('password')
         score = 0
 
         try:
-            user = User.objects.get(username=username)
-            return render(request, 'ctf/register.html', {'error': "Username Has Already Been Taken"})
+            User.objects.get(username=username)
+            return render(request, 'ctf/register.html', {'error': "Username already exists"})
         except User.DoesNotExist:
             user = User.objects.create_user(username=username, password=password)
-            # time = timer()
-            userprofile = UserProfile(user=user, Rid=recid, score=score)
-            userprofile.save()
-            # timer()
+            user_profile = UserProfile.objects.create(user=user, Rid=rec_id, score=score)
             login(request, user)
-
-            return redirect("inst")
+            return redirect("instructions")
 
     elif request.method == 'GET':
         return render(request, 'ctf/register.html')
 
 @csrf_exempt
-def login1(request):
+def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -163,29 +131,26 @@ def login1(request):
 
         if user is not None:
             auth.login(request, user)
-            userprofile = UserProfile.objects.get(user=user)
-            userprofile.time = timer()
-            userprofile.save()
-            return redirect("inst")
+            user_profile = UserProfile.objects.get(user=user)
+            user_profile.time = timer()
+            user_profile.save()
+            return redirect("instructions")
         else:
             messages.error(request, 'Invalid credentials!')
 
     return render(request, 'ctf/login.html')
 
 @csrf_exempt
-def Quest(request):
-    var = calc()
-    if var != 0:
+def quest(request):
+    remaining_time = calc()
+    if remaining_time != "0":
         user = User.objects.get(username=request.user.username)
-        userprofile = UserProfile.objects.get(user=user)
+        user_profile = UserProfile.objects.get(user=user)
         questions = Questions.objects.all().order_by('Qid')
-        submission = Submission.objects.values().filter(user=userprofile).order_by('question_id')
-        print(submission)
-
-        return render(request, 'ctf/quests.html',
-                      {'questions': questions, 'userprofile': userprofile, 'time': var, 'submission': submission})
+        submissions = Submission.objects.filter(user=user_profile).order_by('question_id')
+        return render(request, 'ctf/quests.html', {'questions': questions, 'userprofile': user_profile, 'time': remaining_time, 'submission': submissions})
     else:
-        return HttpResponse("time is 0:0")
+        return HttpResponse("Time is up!")
 
 @csrf_exempt
 def logout(request):
@@ -194,44 +159,9 @@ def logout(request):
 
 @csrf_exempt
 def leaderboard(request):
-    # data = Submission.objects.all().order_by("-curr_score", "-sub_time")
-    sorteduser = UserProfile.objects.all().order_by("-score","latest_sub_time")
-    sub = Submission.objects.values().order_by('-user__score', 'user', 'sub_time')
-    print(sub)
-    count = 4
-    sub_list = []
-    for element in sorteduser:
-        if count <= 4:
-            sub = Submission.objects.values().filter(user_id=element.id)
-            # sub.submission_set.all()
-            # print(sub.submission_set)
-            sub_list.append(sub)
-            print(sub_list)
-            count -= 1
-        else:
-            return render(request, 'ctf/hackerboard.html', context={'sub': sub_list, 'user': sorteduser})
-
-    return render(request, 'ctf/hackerboard.html', context={'sub': sub_list, 'user': sorteduser})
-
-
-'''''def first(request):
-    var = calc()
-    if var != 0:
-        return render(request, 'ctf/.html', context={'time': var})
-    else:
-        return HttpResponse("time is 0:0")'''
-# def leaderboard(request):
-#     #data = Submission.objects.all().order_by("-curr_score", "-sub_time")
-#     sorteduser = UserProfile.objects.all().order_by("-score")
-# if user is not None:
-#             auth.login(request, user)
-#             try:
-#                 userprofile = UserProfile.objects.get(user=user)
-#                 userprofile.time = timer()
-#                 userprofile.save()
-#                 return redirect("inst")
-#             except user.DoesNotExist:
-#                 return render(request, 'CTF/404.html')
-#
-#         else:
-#             return render(request, 'ctf/404.html')
+    sorted_users = UserProfile.objects.order_by("-score", "latest_sub_time")[:5]
+    user_submissions = []
+    for user in sorted_users:
+        submissions = Submission.objects.filter(user=user)
+        user_submissions.append(submissions)
+    return render(request, 'ctf/hackerboard.html', context={'sub': user_submissions, 'user': sorted_users})
