23376eda60a445349d0cc88b1c2363bcbc60c612
Reyansh17 <gahlotreyansh17@gmail.com>
2024-04-16 14:03:47 +0530

    BABA ADAM

--- Iete_ctf/IETE_CTF/IETE_CTF/settings.py
+++ Iete_ctf/IETE_CTF/IETE_CTF/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9p$hk*4y-arh32gfsnnhhx$wral@@*(vvzt%qaj5a%rr5ikc#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
